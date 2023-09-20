67935d6f49f98e51fc599564d40f07418ef4e19d
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-09-20 04:17:31 -0600

    update golang

--- Proyectos/Proyecto_1/Backend/golang/main.go
+++ Proyectos/Proyecto_1/Backend/golang/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"paquetes/models"
 	"paquetes/modules"
 	"paquetes/routes"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -13,6 +18,8 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
+
+var firstTime = true
 
 func main() {
 
@@ -26,11 +33,12 @@ func main() {
 	go func() {
 		tickerRAM := time.NewTicker(1 * time.Second)
 		tickerCPU := time.NewTicker(1 * time.Second)
-		tickerIP := time.NewTicker(5 * time.Second)
-		tickerExtraInfo := time.NewTicker(2 * time.Second)
+		tickerIP := time.NewTicker(1 * time.Second)
+		tickerExtraInfo := time.NewTicker(1 * time.Second)
 		defer tickerRAM.Stop()
 		defer tickerCPU.Stop()
 		defer tickerIP.Stop()
+		defer tickerExtraInfo.Stop()
 		for {
 			select {
 			case <-tickerRAM.C:
@@ -45,6 +53,21 @@ func main() {
 		}
 	}()
 
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// Espera una señal del sistema operativo.
+		sig := <-sigChan
+		fmt.Printf("Recibida la señal %v. Enviando solicitud al servidor...\n", sig)
+
+		// Realiza la solicitud al servidor aquí.
+		delMyInfo()
+
+		// Finaliza el programa después de enviar la solicitud.
+		os.Exit(0)
+	}()
+
 	// Rutas
 	r := mux.NewRouter()
 	routes.IndexRoutes(r)
@@ -56,7 +79,57 @@ func main() {
 	http.ListenAndServe(":3000", httplogger.Golog(corsHandler))
 }
 
-func giveMyInfo(){
+func giveMyInfo() {
+
+	if firstTime {
+		firstTime = false
+		return
+	}
+
+    err2 := godotenv.Load()
+    if err2 != nil {
+        fmt.Println("Error loading .env file")
+    }
+
+    ipPlataform := os.Getenv("IP_PLATAFORM")
+    url := "http://" + ipPlataform + "/setIP"
+
+    temp := models.InfoModulesData{}
+    temp.InfoRAM = modules.InfoRAM
+    temp.InfoCPU = modules.InfoCPU
+    temp.InfoCPU.CPUPercentage = modules.PercentCPU
+    temp.InfoCPU.NameCPU = modules.NameCPU
+
+    // Configura un cliente HTTP con timeout
+    client := &http.Client{
+        Timeout: time.Second * 1, // Aquí puedes ajustar el valor del timeout según tus necesidades
+    }
+
+    // Convierte temp a JSON
+    jsonData, err := json.Marshal(temp)
+    if err != nil {
+        fmt.Println("Error al convertir a JSON:", err)
+        return
+    }
+
+    // Realiza la solicitud POST con el cliente personalizado
+    resp, err := client.Post(url, "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        fmt.Println("Error al hacer la petición:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    // Verifica si la solicitud fue exitosa
+    if resp.StatusCode != http.StatusOK {
+        fmt.Println("La solicitud POST no fue exitosa. Código de estado:", resp.StatusCode)
+        return
+    }
+
+    fmt.Println("Solicitud POST exitosa")
+}
+
+func delMyInfo(){
 	err2 := godotenv.Load()
 	if err2 != nil {
 		fmt.Println("Error loading .env file")
@@ -64,7 +137,7 @@ func giveMyInfo(){
 	
 	ipPlataform := os.Getenv("IP_PLATAFORM")
 
-	url := "http://" + ipPlataform + "/setIP"
+	url := "http://" + ipPlataform + "/delIP"
 	client := &http.Client{
         Timeout: time.Second * 1,
     }
